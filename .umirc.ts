8765366b263fcb101121b7f5f6fed790c0a6d9d0
zhangzequn <zhangzequn@CN3614000295M.local>
2021-06-25 16:55:21 +0800

    add tailwind intergrate and config

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,10 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  tailwindcss: {
+    tailwindCssFilePath: '@/styles/tailwind.css',
+    tailwindConfigFilePath: 'tailwind.config.js', // 默认取值 tailwindConfigFilePath || join(process.env.APP_ROOT || api.cwd, 'tailwind.config.js'),
+  },
 });
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.ts'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '768px',
+      md: '1024px',
+    },
+    borderRadius: {
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
+      5: '20px',
+      6: '24px',
+      full: '9999px',
+    },
+    maxWidth: {
+      '1/2': '50%',
+      '1/3': '33.33%',
+      '2/3': '66.66%',
+      '1/4': '25%',
+      '3/4': '75%',
+      sm: '720px', //  768 - 24 - 24 = 720
+      md: '976px', // 1024 - 24 - 24 = 976
+    },
+    spacing: {
+      px: '1px',
+      1: '4px',
+      2: '8px',
+      3: '12px',
+      4: '16px',
+      5: '20px',
+      6: '24px',
+      8: '32px',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
