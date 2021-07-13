c0d50f0d499f4a9a4faf3ba96a3d84a2ea6fa4ec
zhangzequn <776021361@qq.com>
2021-07-13 20:14:53 +0800

    use predefine router

--- config/config.ts
+++ config/config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'umi';
-import routes from './routes';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes,
   fastRefresh: {},
   tailwindcss: {
     tailwindCssFilePath: '@/styles/tailwind.css',
---
+++ src/pages/homepage/_layout.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Layout(props: any) {
+  return <div>{props.children}</div>;
+}
